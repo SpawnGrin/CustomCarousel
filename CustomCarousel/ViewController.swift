de4a6a50f33335eb9679dbb8e0d7c5c8d0bf4006
Andrey Grinevich <Grinevich.Andrey.Work@gmail.com>
2020-03-09 13:42:37 +0200

    Initial Commit

---
+++ CustomCarousel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCarousel
+//
+//  Created by Andrey Grinevich on 09.03.2020.
+//  Copyright © 2020 Andrey Grinevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
